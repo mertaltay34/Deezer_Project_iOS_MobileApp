61ec4abffd7192ea2e50f6bcaeefe9ccc6c5dcad
Mert <altayme19@itu.edu.tr>
2023-05-10 14:57:29 +0300

    storyboard removed, tab bar controller added

---
+++ Deezer_Project/Controller/FavoriteViewController.swift
@@ -0,0 +1,33 @@
+//
+//  FavoriteViewController.swift
+//  Deezer_Project
+//
+//  Created by Mert Altay on 10.05.2023.
+//
+
+import UIKit
+
+class FavoriteViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
+    // MARK: - HELPERS
+
+extension FavoriteViewController {
+    private func style() {
+        
+    }
+    private func layout() {
+        
+    }
+}
---
+++ Deezer_Project/Controller/MainViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MainViewController.swift
+//  Deezer_Project
+//
+//  Created by Mert Altay on 10.05.2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
+    // MARK: - HELPERS
+
+extension MainViewController{
+    private func style() {
+        
+    }
+    private func layout() {
+        
+    }
+}
